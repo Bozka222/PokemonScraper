ace408a7fdf719b53bb6da2ffe16542cd4713bbb
JanBoch <bochjan@seznam.cz>
2025-12-01 12:47:29 +0100

    Loading products from JSON input

--- src/scraper/__main__.py
+++ src/scraper/__main__.py
@@ -1,21 +1,13 @@
-import time
+import json
+from pathlib import Path
 from scraper.extractor import get_prices
 from scraper.spreadsheet import update_excel
-from pathlib import Path
 
-PRODUCTS = {
-    "Groudon HL102": "https://www.cardmarket.com/en/Pokemon/Products/Singles/EX-Hidden-Legends/Groudon-HL102",
-    "Groudon ADV-P 026": "https://www.cardmarket.com/en/Pokemon/Products/Singles/ADV-Promos/Groudon-V1",
-    "Groudon ADV-P 027": "https://www.cardmarket.com/en/Pokemon/Products/Singles/ADV-Promos/Groudon-V2",
-    "Groudon EM 5": "https://www.cardmarket.com/en/Pokemon/Products/Singles/EX-Emerald/Groudon-EM5",
-    "Groudon pcgF D007": "https://www.cardmarket.com/en/Pokemon/Products/Singles/Gift-Box-Emerald/Groudon-pcgFD007",
-    "Groudon EM 14": "https://www.cardmarket.com/en/Pokemon/Products/Singles/EX-Emerald/Groudon-EM14",
-    "Groudon PPB 007": "https://www.cardmarket.com/en/Pokemon/Products/Singles/PokePark-Blue/Groudon",
-    "Groudon PCG-P 042": "https://www.cardmarket.com/en/Pokemon/Products/Singles/PCG-Promos/Groudon",
-    "Groudon LA 29": "https://www.cardmarket.com/en/Pokemon/Products/Singles/Legends-Awakened/Groudon-Lv45-LA29",
-    "Groudon DP5c": "https://www.cardmarket.com/en/Pokemon/Products/Singles/Cry-from-the-Mysterious/Groudon-Lv45-DP5c",
-}
+PRODUCTS_PATH = Path("products.json")
 
+# Load products from JSON
+with open(PRODUCTS_PATH, "r", encoding="utf-8") as f:
+    PRODUCTS = json.load(f)
 
 def main():
     prices = get_prices(url_dict=PRODUCTS)
