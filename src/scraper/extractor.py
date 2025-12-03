d117c4c2235db9aaf919de4511d6cfc0756e22df
JanBoch <bochjan@seznam.cz>
2025-12-03 16:22:54 +0100

    Test

--- src/scraper/extractor.py
+++ src/scraper/extractor.py
@@ -34,6 +34,11 @@ def get_prices(url_dict: dict[str, str]) -> dict[str, str]:
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument(  # Overrides the headless header to be more real
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0.0.0 Safari/537.36"
+    )
 
     driver = uc.Chrome(options=options)
 
