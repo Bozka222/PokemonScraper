b059893a8000e46d9cef3a14ee215b5b846771a3
JanBoch <bochjan@seznam.cz>
2025-12-03 16:15:01 +0100

    Test

--- src/scraper/extractor.py
+++ src/scraper/extractor.py
@@ -1,3 +1,4 @@
+import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -8,28 +9,33 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_prices(url_dict: dict[str, str]) -> dict[str, str]:
-    options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")  # important for Linux runners
-    options.add_argument("--disable-dev-shm-usage")  # avoid memory issues
-    options.add_argument("--disable-extensions")
-    # options.add_argument('--start-maximized')
-    options.add_argument("--window-size=1920,1080")
-    # options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/120.0.0.0 Safari/537.36"
-    )
+    # options = Options()
+    # options.add_argument("--headless=new") # For newer chrome versions
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--no-sandbox")  # important for Linux runners
+    # options.add_argument("--disable-dev-shm-usage")  # avoid memory issues
+    # options.add_argument("--disable-extensions")
+    # # options.add_argument('--start-maximized') # Does not work with headless
+    # options.add_argument("--window-size=1920,1080")
+    # # options.add_argument("--disable-blink-features=AutomationControlled")
+    # options.add_argument(  # Overrides the headless header to be more real
+    #     "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    #     "AppleWebKit/537.36 (KHTML, like Gecko) "
+    #     "Chrome/120.0.0.0 Safari/537.36"
+    # )
 
     # options.binary_location = "/usr/bin/chromium-browser"  # important for GitHub Actions
-
     # service = Service('/usr/local/bin/chromedriver')
-    # options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    # options.add_experimental_option("useAutomationExtension", False)
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    options = uc.ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = uc.Chrome(options=options)
+
     prices = {}
 
     for name, url in url_dict.items():
